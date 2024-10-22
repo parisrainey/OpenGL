17109112a3f80b15f3b166a858caf237488bdb23
Paris Rainey <paris.c.rainey@gmail.com>
2024-10-22 11:52:29 -0500

    Update: Adding debug

--- OpenGL/Source/Application.cpp
+++ OpenGL/Source/Application.cpp
@@ -1,4 +1,16 @@
-#include "GLFW/glfw3.h"
+#define GLEW_STATIC
+#include "gl/glew.h"
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+unsigned int severity,
+int length, ;
+
+void GLFWErrorCallback(int error, const char* description)
+{
+    std::cerr << description << "\n";
+}
 
 int main(void)
 {
@@ -16,6 +28,13 @@ int main(void)
         return -1;
     }
 
+    //GLFW error callback
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+    glEnable(GL_DEBUG_OUTPUT);
+    glfwSetErrorCallback(GLFWErrorCallback);
+
+    
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
@@ -23,7 +42,14 @@ int main(void)
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        GLCall(glClear(GL_COLOR_BUFFER_BIT));
+        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+            increment = 0.05f;
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -34,4 +60,4 @@ int main(void)
 
     glfwTerminate();
     return 0;
-}
+} 
